9976dc193c923ac8e6d515bbd556c10ae1a1a607
Ehong Kuo <ehongkuo@gmail.com>
2021-09-22 07:58:14 -0700

    figuring out images

--- resources/javascript/script.js
+++ resources/javascript/script.js
@@ -1,6 +1,6 @@
 
 
-const images = ['profile'];
+const images = ['profile', 'baby', 'highschool', 'church', 'faith', 'tangle', 'house'];
 const pictures = [];
 
 images.forEach(function(key) {
@@ -30,3 +30,59 @@ let eventAssignment = function(image) {
 
 pictures.forEach(eventAssignment);
 
+let picOne = document.getElementById('profile');
+let picTwo = document.getElementById('baby');
+let picThree = document.getElementById('highschool');
+let picFour = document.getElementById('church');
+let picFive = document.getElementById('faith');
+let picSix = document.getElementById('tangle');
+let picSeven = document.getElementById('house');
+
+picTwo.hidden = true;
+picThree.hidden = true;
+picFour.hidden = true;
+picFive.hidden = true;
+picSix.hidden = true;
+picSeven.hidden = true;
+
+picOne.onclick = function() {
+  picTwo.hidden = false;
+  picOne.hidden = true;
+  picTwo.style.transform = "rotate(2deg)";
+}
+
+picTwo.onclick = function() {
+  picThree.hidden = false;
+  picTwo.hidden = true;
+  picThree.style.transform = "rotate(-2deg)";
+}
+
+picThree.onclick = function() {
+  picFour.hidden = false;
+  picThree.hidden = true;
+  picFour.style.transform = "rotate(0deg)";
+}
+
+picFour.onclick = function() {
+  picFive.hidden = false;
+  picFour.hidden = true;
+  picFive.style.transform = "rotate(-1deg)";
+}
+
+picFive.onclick = function() {
+  picSix.hidden = false;
+  picFive.hidden = true;
+  picSix.style.transform = "rotate(3deg)";
+}
+
+picSix.onclick = function() {
+  picSeven.hidden = false;
+  picSix.hidden = true;
+  picSeven.style.transform = "rotate(1deg)";
+}
+
+picSeven.onclick = function() {
+  picOne.hidden = false;
+  picSeven.hidden = true;
+  picOne.style.transform = "rotate(-3deg)";
+}
