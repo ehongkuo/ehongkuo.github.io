aeaca79f244bf076376c5e85440625cc6c09bf48
Ehong Kuo <ehongkuo@gmail.com>
2021-09-25 23:07:54 -0700

    photo gallery reset

--- resources/javascript/script.js
+++ resources/javascript/script.js
@@ -1,4 +1,4 @@
-
+/*
 
 const images = ['profile', 'baby', 'highschool', 'church', 'faith', 'tangle', 'house'];
 const pictures = [];
@@ -30,6 +30,8 @@ let eventAssignment = function(image) {
 
 pictures.forEach(eventAssignment);
 
+*/
+
 let picOne = document.getElementById('profile');
 let picTwo = document.getElementById('baby');
 let picThree = document.getElementById('highschool');
@@ -38,6 +40,14 @@ let picFive = document.getElementById('faith');
 let picSix = document.getElementById('tangle');
 let picSeven = document.getElementById('house');
 
+let capOne = document.getElementById('pCap');
+let capTwo = document.getElementById('bCap');
+let capThree = document.getElementById('hCap');
+let capFour = document.getElementById('cCap');
+let capFive = document.getElementById('fCap');
+let capSix = document.getElementById('tCap');
+let capSeven = document.getElementById('houseCap');
+
 picTwo.hidden = true;
 picThree.hidden = true;
 picFour.hidden = true;
@@ -45,44 +55,66 @@ picFive.hidden = true;
 picSix.hidden = true;
 picSeven.hidden = true;
 
+capTwo.hidden = true;
+capThree.hidden = true;
+capFour.hidden = true;
+capFive.hidden = true;
+capSix.hidden = true;
+capSeven.hidden = true;
+
 picOne.onclick = function() {
   picTwo.hidden = false;
   picOne.hidden = true;
-  picTwo.style.transform = "rotate(2deg)";
+  capTwo.hidden = false;
+  capOne.hidden = true;
+  //picTwo.style.transform = "rotate(2deg)";
 }
 
 picTwo.onclick = function() {
   picThree.hidden = false;
   picTwo.hidden = true;
-  picThree.style.transform = "rotate(-2deg)";
+  capThree.hidden = false;
+  capTwo.hidden = true;
+  //picThree.style.transform = "rotate(-2deg)";
 }
 
 picThree.onclick = function() {
   picFour.hidden = false;
   picThree.hidden = true;
-  picFour.style.transform = "rotate(0deg)";
+  capFour.hidden = false;
+  capThree.hidden = true;
+  //picFour.style.transform = "rotate(0deg)";
 }
 
 picFour.onclick = function() {
   picFive.hidden = false;
   picFour.hidden = true;
-  picFive.style.transform = "rotate(-1deg)";
+  capFive.hidden = false;
+  capFour.hidden = true;
+  //picFive.style.transform = "rotate(-1deg)";
 }
 
 picFive.onclick = function() {
   picSix.hidden = false;
   picFive.hidden = true;
-  picSix.style.transform = "rotate(3deg)";
+  capSix.hidden = false;
+  capFive.hidden = true;
+  //picSix.style.transform = "rotate(3deg)";
 }
 
 picSix.onclick = function() {
   picSeven.hidden = false;
   picSix.hidden = true;
-  picSeven.style.transform = "rotate(1deg)";
+  capSeven.hidden = false;
+  capSix.hidden = true;
+  //picSeven.style.transform = "rotate(1deg)";
 }
 
 picSeven.onclick = function() {
   picOne.hidden = false;
   picSeven.hidden = true;
-  picOne.style.transform = "rotate(-3deg)";
+  capOne.hidden = false;
+  capSeven.hidden = true;
+  //picOne.style.transform = "rotate(-3deg)";
+
 }
